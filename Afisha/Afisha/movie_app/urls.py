0eb27c33d4e15a0804cc3183b30456f91a3cabd2
Doni <daakmed8890@gmail.com>
2024-11-05 20:21:16 +0600

    Add files via upload

---
+++ Afisha/Afisha/movie_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DirectorViewSet, MovieViewSet, ReviewViewSet
+
+
+router = DefaultRouter()
+router.register(r'directors', DirectorViewSet)
+router.register(r'movies', MovieViewSet)
+router.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
